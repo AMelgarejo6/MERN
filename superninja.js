14d224ba7d40bf7f2461719802bdee72deddef49
Amelgarejo6 <imsoyanthony6@gmail.com>
2022-10-25 10:31:30 -0700

    "classes" javascript practice

---
+++ superninja.js
@@ -0,0 +1,39 @@
+//parent Ninja class
+class Ninja {
+    constructor(name, health){
+        this.name = name;
+        this.health = health;
+        this.speed = 3;
+        this.strength = 3;
+    }
+
+    sayName() {
+        console.log(`Ninja's name is ${this.name}`);
+    }
+    showStats(){
+        console.log(`Here are ${this.name}'s stats:\nStrength: ${this.strength}\nSpeed: ${this.speed}\nHealth: ${this.health}`);
+    }
+    drinkSake(){
+        this.health += 10;
+    }
+}
+
+//child Sensi class
+class Sensei extends Ninja {
+    constructor(name){
+        super(name);
+        this.health = 200;
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+
+    speakWisdom(){
+        const drink = super.drinkSake();
+        console.log("What one programmer can do in one month, two programmers can do in two months.");
+    }
+}
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
